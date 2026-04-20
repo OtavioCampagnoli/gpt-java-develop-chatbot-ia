3fd59e3ce11399560a4cc2520977b829f3332c92
Otavio Campagnoli <otaviocampagnoli@hotmail.com>
2026-04-20 08:48:13 -0300

    feature: add messages stream

--- src/main/java/br/com/alura/ecomart/chatbot/web/controller/ChatController.java
+++ src/main/java/br/com/alura/ecomart/chatbot/web/controller/ChatController.java
@@ -1,14 +1,22 @@
 package br.com.alura.ecomart.chatbot.web.controller;
 
 import br.com.alura.ecomart.chatbot.web.dto.PerguntaDto;
+import br.com.alura.ecomart.chatbot.web.service.ChatbotService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
 @Controller
 @RequestMapping({"/", "chat"})
 public class ChatController {
 
     private static final String PAGINA_CHAT = "chat";
+
+    private final ChatbotService service;
+
+    public ChatController(ChatbotService service) {
+        this.service = service;
+    }
 
     @GetMapping
     public String carregarPaginaChatbot() {
@@ -17,8 +25,19 @@ public class ChatController {
 
     @PostMapping
     @ResponseBody
-    public String responderPergunta(@RequestBody PerguntaDto dto) {
-        return dto.pergunta();
+    public ResponseBodyEmitter responderPergunta(@RequestBody PerguntaDto dto) {
+        var fluxoDeRespostas = service.responderPergunta(dto.pergunta());
+        var emitter = new ResponseBodyEmitter();
+
+        fluxoDeRespostas.subscribe(chuck -> {
+            var token = chuck.getChoices().get(0).getMessage().getContent();
+
+            if (token != null) {
+                emitter.send(token);
+            }
+        }, emitter::completeWithError, emitter::complete);
+
+        return emitter;
     }
 
     @GetMapping("limpar")
