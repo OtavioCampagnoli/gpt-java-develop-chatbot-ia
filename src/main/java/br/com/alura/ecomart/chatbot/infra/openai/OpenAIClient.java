51c25e235e1444e39f08aa110b4f55c6f907efa2
Otavio Campagnoli <otaviocampagnoli@hotmail.com>
2026-04-22 07:27:22 -0300

    feature: add OpenAI API integration with freight calculation functionality

--- src/main/java/br/com/alura/ecomart/chatbot/infra/openai/OpenAIClient.java
+++ src/main/java/br/com/alura/ecomart/chatbot/infra/openai/OpenAIClient.java
@@ -1,9 +1,18 @@
 package br.com.alura.ecomart.chatbot.infra.openai;
 
+import br.com.alura.ecomart.chatbot.domain.DadosCalculoFrete;
+import br.com.alura.ecomart.chatbot.domain.service.CalculadorDeFrete;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.theokanning.openai.completion.chat.ChatFunction;
+import com.theokanning.openai.completion.chat.ChatFunctionCall;
 import com.theokanning.openai.completion.chat.ChatMessageRole;
 import com.theokanning.openai.messages.Message;
 import com.theokanning.openai.messages.MessageRequest;
+import com.theokanning.openai.runs.Run;
 import com.theokanning.openai.runs.RunCreateRequest;
+import com.theokanning.openai.runs.SubmitToolOutputRequestItem;
+import com.theokanning.openai.runs.SubmitToolOutputsRequest;
+import com.theokanning.openai.service.FunctionExecutor;
 import com.theokanning.openai.service.OpenAiService;
 import com.theokanning.openai.threads.ThreadRequest;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,12 +32,14 @@ public class OpenAIClient {
     private final OpenAiService service;
     private final String assistantId;
     private String threadId;
+    private final CalculadorDeFrete calculadorDeFrete;
 
 
-    public OpenAIClient(@Value("${app.openai.api.key}") String apiKey, @Value("${app.openai.assistant.id}") String assistantId) {
+    public OpenAIClient(@Value("${app.openai.api.key}") String apiKey, @Value("${app.openai.assistant.id}") String assistantId, CalculadorDeFrete calculadorDeFrete) {
         this.apiKey = apiKey;
         this.assistantId = assistantId;
         this.service = new OpenAiService(apiKey, Duration.ofSeconds(60));
+        this.calculadorDeFrete = calculadorDeFrete;
     }
 
     public String enviarRequisicaoChatCompletion(DadosRequisicaoChatCompletion dados) {
@@ -43,6 +54,7 @@ public class OpenAIClient {
                     .builder()
                     .messages(Arrays.asList(messageRequest))
                     .build();
+
             var thread = service.createThread(threadRequest);
             this.threadId = thread.getId();
         } else {
@@ -53,21 +65,51 @@ public class OpenAIClient {
                 .builder()
                 .assistantId(assistantId)
                 .build();
-
         var run = service.createRun(threadId, runRequest);
 
+        var concluido = false;
+        var precisaChamarFuncao = false;
         try {
-            while (!run.getStatus().equalsIgnoreCase("completed")) {
+            while (!concluido && !precisaChamarFuncao) {
                 Thread.sleep(1000 * 10);
                 run = service.retrieveRun(threadId, run.getId());
+                concluido = run.getStatus().equalsIgnoreCase("completed");
+                precisaChamarFuncao = run.getRequiredAction() != null;
             }
-        } catch (Exception e) {
+        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
-        var messages = service.listMessages(threadId);
+        if (precisaChamarFuncao) {
+            var precoDoFrete = chamarFuncao(run);
+            var submitRequest = SubmitToolOutputsRequest
+                    .builder()
+                    .toolOutputs(Arrays.asList(
+                            new SubmitToolOutputRequestItem(
+                                    run
+                                            .getRequiredAction()
+                                            .getSubmitToolOutputs()
+                                            .getToolCalls()
+                                            .get(0)
+                                            .getId(),
+                                    precoDoFrete)
+                    ))
+                    .build();
+            service.submitToolOutputs(threadId, run.getId(), submitRequest);
 
-        return messages
+            try {
+                while (!concluido) {
+                    Thread.sleep(1000 * 10);
+                    run = service.retrieveRun(threadId, run.getId());
+                    concluido = run.getStatus().equalsIgnoreCase("completed");
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        var mensagens = service.listMessages(threadId);
+        return mensagens
                 .getData()
                 .stream()
                 .sorted(Comparator.comparingInt(Message::getCreatedAt).reversed())
@@ -92,8 +134,24 @@ public class OpenAIClient {
         return mensagens;
     }
 
+    private String chamarFuncao(Run run) {
+        try {
+            var funcao = run.getRequiredAction().getSubmitToolOutputs().getToolCalls().get(0).getFunction();
+            var funcaoCalcularFrete = ChatFunction.builder()
+                    .name("calcularFrete")
+                    .executor(DadosCalculoFrete.class, d -> calculadorDeFrete.calcular(d))
+                    .build();
+
+            var executorDeFuncoes = new FunctionExecutor(Arrays.asList(funcaoCalcularFrete));
+            var functionCall = new ChatFunctionCall(funcao.getName(), new ObjectMapper().readTree(funcao.getArguments()));
+            return executorDeFuncoes.execute(functionCall).toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public void apagarThread() {
-        if(this.threadId != null) {
+        if (this.threadId != null) {
             service.deleteThread(threadId);
             this.threadId = null;
         }
